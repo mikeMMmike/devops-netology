79db84ece2ea54840c24c2f9d4ea0e8efb4243ee
Михаил Синица <kish_forever@bk.ru>
2022-07-02 01:17:12 +0500

    07-terraform-03-basic yc тести

--- 07-terraform-03-basic/src/terraform/main.tf
+++ 07-terraform-03-basic/src/terraform/main.tf
@@ -1,50 +1,75 @@
-resource "yandex_compute_instance_group" "group1" {
-  name                = "test-ig"
-  folder_id           = ""
-  service_account_id  = ""
-  deletion_protection = true
-  instance_template {
-    platform_id = local.yc_instance_type_map[terraform.workspace]
-    resources {
-      memory = 2
-      cores  = local.yc_cores[terraform.workspace]
-      instance_count = local.yc_instance_count[terraform.workspace]
-    }
-    boot_disk {
-      mode = "READ_WRITE"
-      initialize_params {
-        image_id = "fd8f1tik9a7ap9ik2dg1"
-        size     = local.yc_disk_size[terraform.workspace]
+/*
+module "vpc" {
+  source  = "hamnsk/vpc/yandex"
+  version = "0.5.0"
+  description = "managed by terraform"
+  create_folder = length(var.yc_folder_id) > 0 ? false : true
+  name = terraform.workspace
+  subnets = local.vpc_subnets[terraform.workspace]
+}
+*/
+
+
+module "news" {
+  source = "./modules/instance"
+  instance_count = local.news_instance_count[terraform.workspace]
+
+  subnet_id     = module.vpc.subnet_ids[0]
+  zone = var.yc_region
+  folder_id = module.vpc.folder_id
+  image         = "centos-7"
+  platform_id   = "standard-v2"
+  name          = "news"
+  description   = "News App Demo"
+  instance_role = "news,balancer"
+  users         = "centos"
+  cores         = local.news_cores[terraform.workspace]
+  boot_disk     = "network-ssd"
+  disk_size     = local.news_disk_size[terraform.workspace]
+  nat           = "true"
+  memory        = "2"
+  core_fraction = "100"
+  depends_on = [
+    module.vpc
+  ]
+}
+
+
+locals {
+  news_cores = {
+    stage = 2
+    prod = 2
+  }
+  news_disk_size = {
+    stage = 20
+    prod = 40
+  }
+  news_instance_count = {
+    stage = 1
+    prod = 2
+  }
+  vpc_subnets = {
+    stage = [
+      {
+        "v4_cidr_blocks": [
+          "10.128.0.0/24"
+        ],
+        "zone": var.yc_region
       }
-    }
-    network_interface {
-      network_id = "yandex_vpc_network.network-1.id"
-      subnet_ids = ["yandex_vpc_subnet.subnet-1.id"]
-    }
-    labels = {
-      label1 = "label1-value"
-      label2 = "label2-value"
-    }
-    metadata = {
-      foo      = "bar"
-      ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
-    }
-    network_settings {
-      type = "STANDARD"
-    }
-  }
-  allocation_policy {
-    zones = ["ru-central1-a"]
-  }
-  deploy_policy {
-    max_unavailable = 2
-    max_creating    = 2
-    max_expansion   = 2
-    max_deleting    = 2
-  }
-  scale_policy {
-    fixed_scale {
-      size = 1
-    }
+    ]
+    prod = [
+      {
+        zone           = "ru-central1-a"
+        v4_cidr_blocks = ["10.128.0.0/24"]
+      },
+      {
+        zone           = "ru-central1-b"
+        v4_cidr_blocks = ["10.129.0.0/24"]
+      },
+      {
+        zone           = "ru-central1-c"
+        v4_cidr_blocks = ["10.130.0.0/24"]
+      }
+    ]
   }
 }
