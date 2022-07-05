58221f32393c353a7522027743b102ca2cd89b24
Михаил Синица <kish_forever@bk.ru>
2022-07-06 03:10:41 +0500

    07-terraform-03-basic Готово 1

--- 07-terraform-03-basic/src/terraform/main.tf
+++ 07-terraform-03-basic/src/terraform/main.tf
@@ -1,52 +1,30 @@
-/*
-module "vpc" {
-  source  = "hamnsk/vpc/yandex"
-  version = "0.5.0"
-  description = "managed by terraform"
-  create_folder = length(var.yc_folder_id) > 0 ? false : true
-  name = terraform.workspace
-  subnets = local.vpc_subnets[terraform.workspace]
+resource "yandex_compute_instance" "vm-1" {
+  name = "terraform1"
+  platform_id = local.yc_instance_type_map[terraform.workspace]
+  /*
+  #нет здесь параметра instance_count
+  instance_count = local.yc_instance_count[terraform.workspace]*/
+  count = local.yc_instance_count[terraform.workspace]
+  resources {
+    cores  = local.yc_cores[terraform.workspace]
+    memory = 2
+ /*   disk_size = local.yc_disk_size[terraform.workspace]*/
+  }
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd8f1tik9a7ap9ik2dg1"
+    }
+  }
+
+  network_interface {
+    /*subnet_id = yandex_vpc_subnet.subnet-1.id*/
+    subnet_id = local.vpc_subnets[terraform.workspace]
+    nat       = true
+  }
+
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
 }
-*/
 
-
-/*module "news" {
-  source = "./modules/instance"
-  instance_count = local.news_instance_count[terraform.workspace]
-
-  subnet_id     = module.vpc.subnet_ids[0]
-  zone = var.yc_region
-  folder_id = module.vpc.folder_id
-  image         = "centos-7"
-  platform_id   = "standard-v2"
-  name          = "news"
-  description   = "News App Demo"
-  instance_role = "news,balancer"
-  users         = "centos"
-  cores         = local.news_cores[terraform.workspace]
-  boot_disk     = "network-ssd"
-  disk_size     = local.news_disk_size[terraform.workspace]
-  nat           = "true"
-  memory        = "2"
-  core_fraction = "100"
-  depends_on = [
-    module.vpc
-  ]
-}*/
-
-
-#locals {
-#/*  news_cores = {
-#    stage = 2
-#    prod = 2
-#  }
-#  news_disk_size = {
-#    stage = 20
-#    prod = 40
-#  }
-#  news_instance_count = {
-#    stage = 1
-#    prod = 2
-#  }*/
-#
-#}
--- 07-terraform-03-basic/src/terraform/network.tf
+++ 07-terraform-03-basic/src/terraform/network.tf
@@ -1,3 +1,54 @@
+#"Мудреная" конфигурация. пытаюсь адаптировать под задачу
+resource "yandex_vpc_network" "network-1" {
+
+  #1 из попыток автоматизировать присвоениe имен:
+  name = "vpc-network-${local.yc_instance_count[terraform.workspace]}"
+
+  #Это не работает, так как "The "count" object can only be used in "module", "resource", and "data" blocks, and only when the "count" argument is set"
+  /*name = "vpc-network-${count.index}"*/
+}
+
+resource "yandex_vpc_subnet" "subnet-1" {
+  count          = "${local.yc_instance_count[terraform.workspace] > length(var.yc_region)}"
+  /*name           = "yc-auto-subnet-${local.yc_instance_count[terraform.workspace]}"*/
+  name           = "yc-auto-subnet-${count.index}"
+  /*
+  #Оригинальный конфиг:
+  name           = "subnet1"*/
+  zone           = local.vpc_subnets[terraform.workspace]
+  /*
+  #Оригинальный конфиг:
+  zone           = "ru-central1-a"*/
+  network_id     = local.vpc_subnets[terraform.workspace]
+  /*network_id     = yandex_vpc_network.network-1.id*/
+  v4_cidr_blocks = ["192.168.10.0/24"]
+}
+
+
+
+
+
+
+
+
+
+
+
+/*
+# Оригинальные параметры из документации Яндекс. Необходимо использовать лишь 1 из конфигураций
+output "internal_ip_address_vm_2" {
+  value = yandex_compute_instance.vm-2.network_interface.0.ip_address
+}
+*/
+#/*
+#output "external_ip_address_vm_2" {
+#  value = yandex_compute_instance.vm-2.network_interface.0.nat_ip_address
+#}
+#*/
+#
+
+/*
+#Еще оригинальные параметры из документации Яндекс.
 # Network
 resource "yandex_vpc_network" "default" {
   name = "net"
@@ -8,4 +59,4 @@ resource "yandex_vpc_subnet" "default" {
   zone           = "ru-central1-a"
   network_id     = "${yandex_vpc_network.default.id}"
   v4_cidr_blocks = ["192.168.101.0/24"]
-}
+}*/
