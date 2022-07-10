b23dfea949fd34f31d757ed279ce0e7b3169a73f
Михаил Синица <kish_forever@bk.ru>
2022-07-11 03:09:03 +0500

    devops-diplom-yandexcloud #2 in progress

---
+++ devops-diplom-yandexcloud/src/terraform/provider.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    yandex = {
+      source  = "yandex-cloud/yandex"
+    }
+  }
+  # Backend settings
+backend "s3" {
+    endpoint   = "storage.yandexcloud.net"
+    bucket     = "netology-bkt"
+    region     = "ru-central1-a"
+    key        = "./****state.tfstate"
+    access_key = "YCAJ******"
+    secret_key = "YCM*******"
+
+    skip_region_validation      = true
+    skip_credentials_validation = true
+}
+
+}
+
+# Provider
+provider "yandex" {
+  token     = "${var.yandex_token}"
+  cloud_id  = "${var.yandex_cloud_id}"
+  folder_id = "${var.yandex_folder_id}"
+  zone      = "${var.yandex_zone_id}"
+}
