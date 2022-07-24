42a100795ab7d32d2184d8582d49baaff4b0feac
Михаил Синица <kish_forever@bk.ru>
2022-07-25 00:42:28 +0500

    devops-diplom-yandexcloud #3Nginx$LetsEncrypt in progress: comment all sites configs, add: inventory.tf

--- devops-diplom-yandexcloud/src/terraform/nginx.tf
+++ devops-diplom-yandexcloud/src/terraform/nginx.tf
@@ -3,23 +3,30 @@ resource "yandex_compute_instance" "nginx" {
   name = "web-server"
   hostname = "mycompanyname.ru"
   platform_id = local.yc_instance_type_map[terraform.workspace]
-  count = local.yc_instance_count[terraform.workspace]
+  /*count = local.yc_instance_count[terraform.workspace]*/
   zone = local.vpc_zone[terraform.workspace]
     resources {
+    core_fraction = 20
     cores  = 2
     memory = 2
+
       }
+  scheduling_policy {
+    preemptible = true
+}
+
   boot_disk {
     initialize_params {
       image_id = "fd8f1tik9a7ap9ik2dg1"
       size = local.yc_disk_size[terraform.workspace]
     }
+
   }
   network_interface {
     subnet_id = yandex_vpc_subnet.yc_subnet.id
     nat       = true
     nat_ip_address = "62.84.118.229"
-    ip_address = "192.168.1.10"
+    ip_address = "192.168.1.12"
   }
   metadata = {
     ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
@@ -27,13 +34,18 @@ resource "yandex_compute_instance" "nginx" {
 }
   resource "null_resource" "ansible-install" {
 
-  triggers = {
-    always_run = "${timestamp()}"
-  }
+    triggers = {
+      always_run = "${timestamp()}"
+    }
 
   provisioner "local-exec" {
-    command = format("ansible-playbook -D -i %s, -u ubuntu ../ansible/nginx/provision.yml",
+    command = format("sleep 40 && ssh-keyscan 62.84.118.229 >> ~/.ssh/known_hosts && ansible-playbook -D -i %s, -u ubuntu ../ansible/nginx/provision.yml",
     join("\",\"", yandex_compute_instance.nginx[*].network_interface.0.nat_ip_address)
     )
   }
+/* Не удалось сформировать корректный inventory файл. посему отложил это решение
+    provisioner "local-exec" {
+      command = format(" sleep 30 && ANSIBLE_FORCE_COLOR=1 ansible-playbook -i ../ansible/inventory ../ansible/nginx/provision.yml" #, join("\",\"", yandex_compute_instance.nginx[*].network_interface.0.nat_ip_address)
+    )
+    }*/
   }
