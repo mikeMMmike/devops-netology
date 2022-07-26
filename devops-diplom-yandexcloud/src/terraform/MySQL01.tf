9f7069457389f6d5bff535d9aafb7155a6354e63
Михаил Синица <kish_forever@bk.ru>
2022-07-26 17:51:40 +0500

    devops-diplom-yandexcloud #3Nginx$LetsEncrypt in progress: maked ansible role from ansible-playbook

---
+++ devops-diplom-yandexcloud/src/terraform/MySQL01.tf
@@ -0,0 +1,41 @@
+resource "yandex_compute_instance" "MySQL01" {
+  name = "MySQL01"
+  hostname = "mycompanyname.ru"
+  platform_id = local.yc_instance_type_map[terraform.workspace]
+  zone = local.vpc_zone[terraform.workspace]
+    resources {
+    core_fraction = 20
+    cores  = 4
+    memory = 4
+      }
+    scheduling_policy {
+    preemptible = true
+}
+
+  boot_disk {
+    initialize_params {
+      image_id = "fd8f1tik9a7ap9ik2dg1"
+      size = local.yc_disk_size[terraform.workspace]
+    }
+
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.yc_subnet.id
+    nat       = false
+    ip_address = "192.168.1.16"
+  }
+  metadata = {
+    ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
+  }
+}
+  /*resource "null_resource" "ansible-install" {
+
+    triggers = {
+      always_run = "${timestamp()}"
+    }
+
+  provisioner "local-exec" {
+    command = format("sleep 40 && ssh-keyscan 62.84.118.229 >> ~/.ssh/known_hosts && ansible-playbook -D -i %s, -u ubuntu ../ansible/nginx/provision.yml",
+    join("\",\"", yandex_compute_instance.nginx[*].network_interface.0.nat_ip_address)
+    )
+  }*/
